436276fb70a1d6bf1d54c64c6890a46a7ee487a6
HWISU96 <soshyzx123@naver.com>
2022-05-23 10:39:49 +0900

    feat : 이미지 미리보기 추가

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,10 @@
 function posting() {
     // let title = $('#title').val()
     let file = $('#file')[0].files[0]
-    let file_title = 'upload'
 
     let form_data = new FormData()
 
-    // form_data.append("title_give", title)
     form_data.append("file_give", file)
-    form_data.append("file_title_give", file_title)
 
     $.ajax({
         type: "POST",
