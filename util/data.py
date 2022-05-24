cfa248b3d85fe41d354a32d65a2e5adc85c16954
sungwookoo <sungwookoo.dev@gmail.com>
2022-05-24 17:18:34 +0900

    test : 테스트용 전체 포켓몬 insert 기능 추가

--- util/data.py
+++ util/data.py
@@ -36,7 +36,34 @@ def make_insert(doc, table):
     db_table.insert_one(doc)
 
 
-for i in range(10):
+labeldn = {"person": "mr-mime", "bicycle": "dodrio", "car": "entei", "motorbike": "dialga", "aeroplane": "latios",
+           "bus": "caterpie", "train": "onix", "truck": "pelipper",
+           "boat": "blastoise", "traffic light": "pikachu", "fire hydrant": "squirtle", "stop sign": "hypno",
+           "parking meter": "psyduck", "bench": "bulbasaur",
+           "bird": "pidgeotto", "cat": "meowth", "dog": "growlithe", "horse": "ponyta", "sheep": "mareep",
+           "cow": "tauros", "elephant": "phanpy", "bear": "ursaring", "zebra": "blitzle", "giraffe": "girafarig",
+           "backpack": "chikorita", "umbrella": "foongus", "handbag": "azurill", "tie": "ekans",
+           "suitcase": "trubbish", "frisbee": "jigglypuff", "skis": "lapras", "snowboard": "articuno",
+           "sports ball": "voltorb", "kite": "mantyke", "baseball bat": "Farfetchd", "baseball glove": "binacle",
+           "skateboard": "chimecho", "surfboard": "cofagrigus", "tennis racket": "cradily", "bottle": "gardevoir",
+           "wine glass": "lileep",
+           "cup": "lugia", "fork": "sigilyph", "knife": "pawniard", "spoon": "kadabra", "bowl": "wobbuffet",
+           "banana": "tropius", "apple": "tsareena", "sandwich": "dugtrio",
+           "broccoli": "budew", "carrot": "lopunny", "hot dog": "diglett", "pizza": "blaziken", "donut": "comfey",
+           "cake": "gothitelle", "chair": "metagross", "sofa": "dewgong", "potted plant": "oddish",
+           "bed": "clefable",
+           "diningtable": "torterra", "toilet": "weepinbell", "tv monitor": "charjabug", "laptop": "torkoal",
+           "mouse": "dedenne",
+           "remote": "umbreon", "keyboard": "kyogre", "cell phone": "mewtwo", "microwave": "rotom",
+           "oven": "paras", "toaster": "snorlax", "sink": "gyarados", "refrigerator": "meltan", "book": "slowpoke",
+           "clock": "noctowl",
+           "vase": "venusaur", "scissors": "scizor", "teddy bear": "teddiursa", "hair drier": "bellsprout",
+           "toothbrush": "poliwag", "orange": "trapinch"}
+
+
+labeldn_values = list(labeldn.values())
+
+for label in labeldn_values:
     poke_li = ["banana", "bird"]
     acc_li = [98.9956200122833, 73.5150396823883]
     doc = {
@@ -44,6 +71,6 @@ for i in range(10):
         'poke_li': poke_li,
         'acc_li': acc_li,
         'created_at': now,
-        'feed_img_src': random_img_src()
+        'feed_img_src': 'static/pokemon/'+label+'.png'
     }
     make_insert(doc, 'feed')
